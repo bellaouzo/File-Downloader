1c17f8354d5d32d37fb49fd9aa257ba71579d8f3
bellaouzo <bellaouzo@gmail.com>
2026-05-04 22:59:59 -0600

    Added powerpoint

--- obj/Debug/net9.0-windows/FileDownloader.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/FileDownloader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileDownloader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0148336dc8497ee16e5255f47427c6bc733cf93")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileDownloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileDownloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
